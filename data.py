3faa75ea23a822168014209e451fd636d8392f94
chunlei.ncl <chunlei.ncl@alibaba-inc.com>
2016-08-29 09:36:19 +0800

    init

--- data.py
+++ data.py
@@ -11,10 +11,10 @@ while True:
             fname = ktime.strftime("%y-%m-%d")
             ktimestr = ktime.strftime("%y-%m-%d %H:%M")
             fwriter = open(store_dir+"/"+fname,"wa+")
-
+            offset = kline.time/24*60*60
+            fwriter.seek(offset)
             #32 + 16 + 2 = 50 + close open high low,time
             fwriter.write("%6.2f,%6.2f,%6.2f,%6.2f,%s\r\n",kline.close,kline.open,kline.high,kline.low,ktimestr)
-
     except Exception as e:
         pass
 
