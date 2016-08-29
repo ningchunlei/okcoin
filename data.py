9d4d1132303359b31424e76ec3aa80e9e45bfdc9
chunlei.ncl <chunlei.ncl@alibaba-inc.com>
2016-08-29 09:40:10 +0800

    init

--- data.py
+++ data.py
@@ -1,21 +1,18 @@
 import Client
 from datetime import datetime
 
-store_dir=""
+store_dir="/tmp"
 
 while True:
-    try:
-        klines = Client.fetchKline("btc","1min",500)
-        for kline in klines:
-            ktime = datetime.fromtimestamp(kline.time)
-            fname = ktime.strftime("%y-%m-%d")
-            ktimestr = ktime.strftime("%y-%m-%d %H:%M")
-            fwriter = open(store_dir+"/"+fname,"wa+")
-            offset = kline.time/24*60*60
-            fwriter.seek(offset)
-            #32 + 16 + 2 = 50 + close open high low,time
-            fwriter.write("%6.2f,%6.2f,%6.2f,%6.2f,%s\r\n",kline.close,kline.open,kline.high,kline.low,ktimestr)
-    except Exception as e:
-        pass
+    klines = Client.fetchKline("btc","1min",500)
+    for kline in klines:
+        ktime = datetime.fromtimestamp(kline.time)
+        fname = ktime.strftime("%y-%m-%d")
+        ktimestr = ktime.strftime("%y-%m-%d %H:%M")
+        fwriter = open(store_dir+"/"+fname,"wa+")
+        offset = kline.time/24*60*60
+        fwriter.seek(offset)
+        #32 + 16 + 2 = 50 + close open high low,time
+        fwriter.write("%6.2f,%6.2f,%6.2f,%6.2f,%s\r\n",kline.close,kline.open,kline.high,kline.low,ktimestr)
 
 
