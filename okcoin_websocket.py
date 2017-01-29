8527166b7f1ebfa05df1523ca0891f9b8d85a7e1
jack <ningchunlei@gmail.com>
2017-01-29 23:10:56 +0800

    int

--- okcoin_websocket.py
+++ okcoin_websocket.py
@@ -5362,7 +5362,7 @@ def go19():
                     distance = stock1Min.checkdistance(buyTriggerTime[2].time)
                     #pricelogging.info("tbuy spec=%s,time=%s,distance=%s,trigger=%s,pre1=%s,pre2=%s,last=%s" % (xspec,time.ctime(lastm1.time),distance,buyTriggerTime,prelastm1,prelastm2,lastm1))
                     if distance == 1:
-                        if prelastm1.macd<prelastm2.macd and prelastm1.macd<lastm1.macd and lastm1.mn["5"]>prelastm1.mn["5"]:
+                        if prelastm1.macd<=prelastm2.macd and prelastm1.macd<lastm1.macd and lastm1.mn["5"]>prelastm1.mn["5"]:
                             xt = (43,buyTriggerTime[1],buyTriggerTime[2],kkdata[0])
                             xspec = None
                             buyTriggerTime = None
